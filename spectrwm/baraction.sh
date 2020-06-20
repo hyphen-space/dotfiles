aa4829a236c7534eafcacbc1721ac9fe0ea33fb9
nbd <banana_red_guy@hotmail.com>
2020-06-19 20:41:18 -0700

    coloured icons in bar

--- spectrwm/baraction.sh
+++ spectrwm/baraction.sh
@@ -1,42 +1,60 @@
 #!/bin/bash
 
+# battery icons
+BATTERY_CHARGING=(           )
+TIME_ICON=
+CAL_ICON=
+VOL_ICONS=(    )
+MUSIC_ICON=
+
 song() {
     song=$(ncmpcpp --current-song='%a - %t')
     if [ -n "$song" ]
     then
-        echo -e "+@fg=1;now playing+@fg=0; $song "
+        echo -e "$MUSIC_ICON $song "
     fi
 }
 
 vol(){
-	vol=$(pulsemixer --get-volume | cut -d ' ' -f 1)
-	mute=$(pulsemixer --get-mute)
-	if [ $mute -eq 0 ]; then
-		echo -e "+@fg=1;vol+@fg=0; $vol% "
-	elif [ $mute -eq 1 ]; then
-		echo -e "+@fg=1;vol+@fg=0; [Mute] "
-	else
-		echo -e "+@fg=1;vol+@fg=0; ??? "
-	fi
+	VOL_INT=$(pulsemixer --get-volume | cut -d ' ' -f 1)
+	VOL_MUTE=$(pulsemixer --get-mute)
+	if [ $VOL_MUTE -eq 1 ]; then
+        VOL_ICON=${VOL_ICONS[0]}
+        VOL_STR=''
+    else
+        if [ $VOL_INT -lt 60 ]; then
+            VOL_ICON=${VOL_ICONS[1]}
+        elif [ $VOL_INT -ge 60 ]; then
+            VOL_ICON=${VOL_ICONS[2]}
+        fi
+	    VOL_INT=$VOL_INT%
+    fi
+
+    echo -e "$VOL_ICON $VOL_INT "
 }
 
 bat() {
-	power=$(cat /sys/class/power_supply/BAT0/capacity)
-	echo -e "+@fg=1;bat+@fg=0; $power% "
+	BAT_PER=$(cat /sys/class/power_supply/BAT0/capacity)
+    BAT_STATE=$(cat /sys/class/power_supply/BAT0/status)
+
+    i=$((BAT_PER/20))
+    BAT_ICON=${BATTERY_CHARGING[$i]}
+
+	echo -e "$BAT_ICON $BAT_PER% "
 }
 
 dte() {
   dte=$(date +"%a %B %d")
-  echo -e "+@fg=1;date+@fg=0; $dte "
+  echo -e "$CAL_ICON $dte "
 }
 
 tie() {
   tme=$(date +"%I:%M%P")
-  echo -e "+@fg=1;time+@fg=0; $tme "
+  echo -e "$TIME_ICON $tme "
 }
 
 while :; do
-    echo "$(song) $(vol) $(bat) $(tie) $(dte)"
-	sleep 2
+    echo "   +@bg=2;  $(song) $(vol) +@bg=0;   +@bg=3;  $(bat) +@bg=0;   +@bg=4;  $(tie) +@bg=0;   +@bg=5;  $(dte) "
+	sleep 1
 done
 
